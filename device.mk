cd14d9412517503af037c637d428c07da3c3cb96
wh0dat <whodat@remainsilent.net>
2020-03-31 06:03:45 +0000

    ocean: thermal: Import thermal configs from stock
    Signed-off-by: wh0dat <whodat@remainsilent.net>

--- device.mk
+++ device.mk
@@ -50,5 +50,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # Inherit from motorola sdm632-common
 $(call inherit-product, device/motorola/sdm632-common/common.mk)
