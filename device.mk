62b856cad1ab8c547b2e3b98ced18b8657894bd1
wh0dat <whodat@remainsilent.net>
2020-03-31 05:57:52 +0000

    ocean: audio: Import audio configs from stock
    Signed-off-by: wh0dat <whodat@remainsilent.net>

--- device.mk
+++ device.mk
@@ -24,6 +24,12 @@ $(call inherit-product-if-exists, vendor/motorola/ocean/ocean-vendor.mk)
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_ext_spkr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_ext_spkr.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1440
 TARGET_SCREEN_WIDTH := 720
