bbd03c84e96e41f422bb72d9d6eac63a0d604114
wh0dat <whodat@remainsilent.net>
2020-03-31 07:36:57 +0000

    ocean: AEX
    Signed-off-by: wh0dat <whodat@remainsilent.net>

--- aosp_ocean.mk
+++ aosp_ocean.mk
@@ -19,21 +19,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit from ocean device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+TARGET_BOOT_ANIMATION_RES := 1080
+
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := ocean
 PRODUCT_MANUFACTURER := motorola
-PRODUCT_NAME := lineage_ocean
+PRODUCT_NAME := aosp_ocean
 PRODUCT_MODEL := moto g(7) power
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
         PRODUCT_NAME=ocean \
-        PRIVATE_BUILD_DESC="ocean_t 10 QPO30.52-5 58b45 release-keys"
+        PRIVATE_BUILD_DESC="ocean-user 9 PPOS29.114-134-10 21fefe release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := motorola/ocean_t/ocean_t:10/QPO30.52-5/58b45:user/release-keys
+BUILD_FINGERPRINT := motorola/ocean/ocean:9/PPOS29.114-134-10/21fefe:user/release-keys
