1834d0ad2f4c2f96b007cd414364bcf292c637bc
Yagya Parihar <yagya.parihar@gmail.com>
2020-05-05 22:36:50 -0600

    Replacing Revenge with Lineage because I'm a peaceful man
    Change-Id: I9ed7fe377ac334dc82cab56dd42d02784a3f55cf

--- device.mk
+++ device.mk
@@ -23,9 +23,8 @@ $(call inherit-product-if-exists, vendor/motorola/ocean/ocean-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage \
-    $(LOCAL_PATH)/overlay-revenge
-
+    $(LOCAL_PATH)/overlay-lineage
+    
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
 
